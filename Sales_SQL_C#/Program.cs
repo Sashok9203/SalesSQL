77fff316d6258b80b3ae364886588434593d36fd
Koval <Koval@DESKTOP-MSEUOFN>
2023-07-18 10:28:56 +0300

    Добавьте файлы проекта.

---
+++ Sales_SQL_C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sales_SQL_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
